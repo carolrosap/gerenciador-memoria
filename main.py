b7092d0ecc9352c53e490cb7576545b8199a3396
Caroline Rosa <cpauladarosa@gmail.com>
2021-06-28 17:56:41 -0300

    alterações menu e validações

--- main.py
+++ main.py
@@ -64,8 +64,6 @@ def update_VM(process, process_pages):
             txt_tmp = txt_tmp + "\n"
         
             for v_page in old_data:
-                #print(v_page)
-                #print(process + str(idx))
                 if v_page == process + str(idx):
                     old_data[v_page] = process_pages[page]
                     red = False
@@ -97,9 +95,10 @@ def update_VM(process, process_pages):
 def run_process(process_to_run):
     with open(PROCESSES_PATH+process_to_run+".txt", 'r') as file:
         #print(file.read())
+       
         process = file.read().replace(";", "").replace(",", "")
         process = process.split("\n")
-        #print(int(process[1])/PAGE_SIZE)
+        #print(process[1])
         
         process_pages = {clebs: [address for address in range(PAGE_SIZE)] for clebs in range(int(process[1])//PAGE_SIZE)}
         #print(process_pages)
@@ -107,7 +106,7 @@ def run_process(process_to_run):
         update_VM(process[0], process_pages)
         for clebs in process[2:]:
             PROCESS_Q.append(str(process[0]) + " " + clebs)
-            
+              
         LOGIC_MEMORY[process[0]] = process_pages
         
     return PROCESS_Q
@@ -138,16 +137,32 @@ def store_address(p_id, value, page_address, frame_address, p_pages):
                 
     print(MAIN_MEMORY[(frame_address*PAGE_SIZE)+tmp])
     MAIN_MEMORY[(frame_address*PAGE_SIZE)+tmp] = value
-    print(MAIN_MEMORY[(frame_address*PAGE_SIZE)+tmp])
+    
                 
     p_pages[int(page_address)//PAGE_SIZE][tmp] = MAIN_MEMORY[(frame_address*PAGE_SIZE)+tmp]
             
     update_VM(p_id, p_pages)
+    
+def sub_addres(p_id, origin_address, dest_address, frame_address, p_pages):
+    tmp = int(dest_address)
+    while tmp >= PAGE_SIZE:
+        tmp = tmp - PAGE_SIZE
+        
+    TLB[frame_address] = p_id + " " + str(int(dest_address)//PAGE_SIZE)
+    PAGES_Q.append(frame_address)
+    #MAIN_MEMORY[(frame_address*PAGE_SIZE)+tmp] = value
+    
+    
+    
 
 def interpret_command(command):
-    #print(command)
+   
     command = command.split(" ")
-    p_pages = LOGIC_MEMORY[command[0]]
+    #command[0].strip()
+    print(command[0])
+    
+    print(LOGIC_MEMORY)
+    p_pages = LOGIC_MEMORY[command[0] + " "] #VER PQ ESTAVA COM ERRO DE ESPAÇO AQUI
     
     if command[1] == "l":        
         
@@ -166,17 +181,31 @@ def interpret_command(command):
         for address in TLB:
             if TLB[address] == -1:
                 store_address(command[0], command[2], command[3], address, p_pages)
-                
+                            
                 break
         if PAGE_SUBSTITUTION == 1:
             load_address(command[0], command[2], PAGES_Q[0], p_pages)
-            del PAGES_Q[0]         
+            del PAGES_Q[0]    
+    
+    #elif command[1] == "add":
+        
+    #elif command[1] == "sub":
+    
+    #elif command[1] == "sa":
+     #   for address in TLB:
+      #      if TLB[address] == -1:  
+              
+       
+       
+#Início
 
-MM_SIZE = int(input("> Insira o tamanho da memória pricnipal: "))
+MM_SIZE = int(input("> Insira o tamanho da memória principal: "))
 
 PAGE_SIZE = int(input("\n> Insira o tamanho das páginas: "))
 
-PAGE_SUBSTITUTION = input("\n> Insira o algorítmo que deseja utilizar: ")
+PAGE_SUBSTITUTION = input("\n> Insira o algorítmo que deseja utilizar:\n1 - FIFO\n2 - LRU\n3 - Segunda chance\n")
+while(PAGE_SUBSTITUTION != '1' and PAGE_SUBSTITUTION != '2' and PAGE_SUBSTITUTION != '3'):
+    PAGE_SUBSTITUTION = input(" > Valor inválido, digite novamente: ")
 # 1 - FIFO
 
 MAIN_MEMORY = {clebs : 0 for clebs in range(MM_SIZE)}
@@ -185,24 +214,32 @@ TLB = {clebs: -1 for clebs in range(MM_SIZE//PAGE_SIZE)}
 
 red = True
 
-while red:
-        
+while red:     
     print(">--------------------\n")
-    print("|\n")
+    #print("|\n")
+    print("\n> Processos disponíveis\n")
+    
     processes = [f for f in listdir(PROCESSES_PATH) if isfile(join(PROCESSES_PATH, f))]
+    
+    list_proc = []
     for proc in processes:
         print("| > " + proc[:-4] + ";\n")
+        list_proc.append(proc[:-4])
     
-    print("|\n")
+    print("> run para rodar")
     print(">--------------------\n")
-
+    
     process_to_run = input("\n> Insira o processo que deseja executar: ")
+   
     if process_to_run == "run":
         red = False
         break
-
+ 
+    while(not(list_proc.__contains__(process_to_run))):    
+       process_to_run = input("> Processo inválido, digite novamente: ")
+        
     PROCESS_Q = run_process(process_to_run)
-#print(PROCESS_Q)
+
 
 for proc in PROCESS_Q:
     interpret_command(proc)
